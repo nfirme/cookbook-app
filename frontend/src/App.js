7225d90278a78ce6211565feb95f1410f36ba1ec
Nick Firme <nickfirme@live.com>
2021-03-21 00:41:14 -0700

    Set up basic app container and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Landing from './components/Landing/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
+    <div className="App" >
+      <Container className="AppContainer">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </Router>
+      </Container>
     </div>
   );
 }
