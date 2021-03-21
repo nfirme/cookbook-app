39d505a5b30205f1ade9cf805133d39a3c111ff0
Nick Firme <nickfirme@live.com>
2021-03-21 13:46:34 -0700

    Added some stuff to the NavBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import logo from '../../images/chef.svg'
+import './NavBar.css'
 
 function NavBar() {
     return (
-        <Navbar bg="dark" variant="dark" style={{height: "48px"}}>
+        <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">
+                    <img
+                        alt="logo"
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top mr-2"
+                    />
+                    cookbook.io
+                </Navbar.Brand>
+                <Nav.Item>
+                    <Nav.Link href="/signup">Sign Up</Nav.Link>
+                </Nav.Item>
         </Navbar>
     )
 }
