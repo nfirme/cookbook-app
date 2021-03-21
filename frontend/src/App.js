278e221b2666d9bf994ee720a8a36e56fa023a45
Nick Firme <nickfirme@live.com>
2021-03-21 13:14:44 -0700

    Added NavBar to App and changed removed container bg color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from 'react-bootstrap/Container'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './components/Landing/Landing'
+import NavBar from './components/NavBar/NavBar.jsx'
 
 function App() {
   return (
     <div className="App" >
+    <NavBar />
       <Container className="AppContainer">
       <Router>
         <Switch>
