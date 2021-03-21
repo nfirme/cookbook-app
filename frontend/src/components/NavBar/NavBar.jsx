64ff43dd7a4e4640480b505a7a7afbb05ccbf99e
Nick Firme <nickfirme@live.com>
2021-03-21 13:13:53 -0700

    Added NavBar skeleton

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+
+function NavBar() {
+    return (
+        <Navbar bg="dark" variant="dark" style={{height: "48px"}}>
+        </Navbar>
+    )
+}
+
+export default NavBar;
