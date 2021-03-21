874e310faed6c708a4e3a63cb4b9c3b2817d450e
Nick Firme <nickfirme@live.com>
2021-03-21 13:13:32 -0700

    Styled Landing page

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Landing.css'
+import chef from '../../images/chef.svg'
+import Button from 'react-bootstrap/Button'
 
 function Landing() {
     return (
-        <div>
-            <h1>Landing</h1>
+        <div className="Landing">
+            <img src={chef} alt="chef" className="chef-icon" />
+            <h1 className="Landing-text">
+                Welcome to <span className="title">cookbook.io</span>
+            </h1>
+            <h4 className="Landing-subtitle">connecting you with your favorite recipes</h4>
+            <Button 
+                variant="outline-primary" 
+                size="lg" 
+                className="pink-btn"
+                onClick={() => { console.log("click"); }}
+            >Sign Up</Button>
         </div>
     )
 }
